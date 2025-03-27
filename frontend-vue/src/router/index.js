9bb27603c6155959eb67c2b0b5393750e48e931c
Francisco Moreno Martinez <franciscomoreno942@outlook.com>
2025-03-27 10:57:57 -0600

    fix(ui): add navbar and core routes.

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import HomeView from '../views/HomeView.vue';
+import CartView from '../views/CartView.vue';
+import CheckoutView from  '../views/CheckoutView.vue';
+import PaymentView from '../views/PaymentView.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,12 +12,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/cart',
+      name: 'cart',
+      component: CartView,
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: CheckoutView,
+    },
+    {
+      path: '/payment',
+      name: 'payment',
+      component: PaymentView,
     },
   ],
 })
---
+++ frontend-vue/src/views/CheckoutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="checkout">
+    <h1>This is checkout view</h1>
+  </div>
+</template>
